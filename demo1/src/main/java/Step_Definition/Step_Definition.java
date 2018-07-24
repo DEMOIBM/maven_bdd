a0768716d939cd44bf3b120c24b76284de64f08e
IBM <IBM@DEVOPS_18>
2018-07-24 15:02:44 +0530

    uploading

---
+++ demo1/src/main/java/Step_Definition/Step_Definition.java
@@ -0,0 +1,31 @@
+package Step_Definition;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Step_Definition {
+	WebDriver ffdriver;
+	@Given("^Login application should be available$")
+	public void login_application_should_be_available()  {
+		
+			ffdriver = new FirefoxDriver();
+		    ffdriver.get("http://opensource-demo.ornagehrmlive.com");
+		      
+	}
+
+	@When("^Validate Login application$")
+	public void validate_Login_application()  {
+		String title = ffdriver.getTitle();
+		   System.out.println(title);
+	    
+	}
+
+	@Then("^Login should be successful$")
+	public void login_should_be_successful() {
+	}
+	
+
+
+
+}
