72d1f8d8baba14dc174fa28af07129f60999f57e
IBM <IBM@DEVOPS_18>
2018-07-25 12:32:45 +0530

    Uploading...

--- demo1/src/main/java/Step_Definition/Step_Definition.java
+++ demo1/src/main/java/Step_Definition/Step_Definition.java
@@ -1,8 +1,12 @@
 package Step_Definition;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.When;
+import cucumber.api.java.en.Then;
 import org.openqa.selenium.firefox.FirefoxDriver;
+
+
 
 public class Step_Definition {
 	WebDriver ffdriver;
